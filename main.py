bee06901ed3e8336724b14bd5beda37cba60dd57
asibai7 <135031679+asibai7@users.noreply.github.com>
2023-06-02 00:18:46 +0000

    Edited code for bugs
    Fixed all of SQL queries

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from objects import System
-from objects import Student
 def loginMenu(system):
   loggedOn = False
-  print("Welcome to the InCollege login page!\n")
+  print("Welcome to the InCollege home page!\n")
   choice = input("[1] Login [2] Create Account [0] Exit\n")
-  while(choice != 0 and loggedOn == False):
+  while(choice != "0" and loggedOn == False):
     if(choice == "1"):
-      username = input("Enter User Name: ")
+      username = input("Enter Username: ")
       password = input("Enter Password: ")
-      loggedOn = system.login(username, password)
+      system.login(username, password)
+      loggedOn = True
     elif(choice == "2"):
-      username = input("Enter User Name: ")
+      username = input("Enter Username: ")
       password = input("Enter Password: ")
       passwordCheck = input("Confirm Password: ")
-      loggedOn = system.register(username, password, passwordCheck)
-    print("Incorrect input. Please try again.")
-    choice = input("[1] Login [2] Create Account [0] Exit\n")
-    
+      system.register(username, password, passwordCheck)
+    else:
+      print("Incorrect input. Please try again.")
+      choice = input("[1] Login [2] Create Account [0] Exit\n")
 system = System() #creating instance of System
 loginMenu(system)   
--- objects.py
+++ objects.py
@@ -1,5 +1,6 @@
 import sqlite3
 import re
+
 class System:
   def __init__(self): #create and connect to db
     self.conn = sqlite3.connect("accounts.db") #establishes connection to SQLite database called accounts
@@ -22,23 +23,24 @@ class System:
     return True
 
   def login(self, username, password): #login check
-    ## Sanitize the Inputs Here
-      self.cursor.execute("SELECT * FROM accounts WHERE username = ?" + username) 
+      self.cursor.execute("SELECT * FROM accounts WHERE username = ?", (username,)) 
       #? is placeholder for username
       account = self.cursor.fetchone() #fetches first row which query returns
       if account: #if the username exists, then we check that the password in the database matches the password the user inputted
-        if account[2] == password:
+        if account[1] == password:
           print("You have successfully logged in!")
+        else:
+          print("Invalid username/password, try again!")
       else:
         print("Account not found, check credentials.")
       
   def register(self, username, password, password_check):
     if password != password_check:
       print("Passwords must match.")
-    if not self.validate(password):
+    elif not self.validate(password):
       print("Password must have 8-12 characters, at least one capital letter, one digit, and one special character.")
-    while True:
-      self.cursor.execute("SELECT * FROM accounts WHERE username=?(username)", (username))
+    else:
+      self.cursor.execute("SELECT * FROM accounts WHERE username=?", (username,))
       exists_user = self.cursor.fetchone()
       if exists_user:
         print("Username has been taken.")
@@ -50,4 +52,3 @@ class System:
 class Student:
   def __init__(self, userName):
     self.userName = userName
- 
