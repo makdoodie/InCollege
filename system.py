fbe0fa59ce4a4da7b4d25eb59039395cb6109f8d
makdoodie <45640071+makdoodie@users.noreply.github.com>
2023-06-17 03:24:29 +0000

    Added InCollege Important Links

--- system.py
+++ system.py
@@ -99,6 +99,7 @@ class System:
     self.videoMenu = Menu()
     self.skillsMenu = Menu()
     self.joinMenu = Menu()
+    self.importantLinks = Menu()
     
   def __del__(self): #closes connection to db
     self.conn.close()
@@ -128,6 +129,8 @@ class System:
       self.videoMenu.start()
   def skills_menu(self):
       self.skillsMenu.start()
+  def important_links(self):
+      self.importantLinks.start()
     
   def encryption(self, password):
     sha256 = hashlib.sha256()
@@ -313,6 +316,173 @@ class System:
         
     else:
       print("Invalid Name Given. Please Try Again.")
+
+  ## Function for the important links to print
+  def printLink(self, link):
+        content = {
+            'Copyright Notice': '''
+---------------------------
+      COPYRIGHT NOTICE
+---------------------------
+
+All content and materials displayed on the InCollege website, including but not limited to text, graphics, logos, images, audio clips, and software, are the property of InCollege and are protected by international copyright laws.
+
+The unauthorized reproduction, distribution, or modification of any content on the InCollege website is strictly prohibited without prior written permission from InCollege.
+
+For any inquiries regarding the use of our copyrighted materials, please contact us at legal@incollege.com.
+
+By accessing and using the InCollege website, you agree to comply with all applicable copyright laws and regulations.
+
+---------------------------
+''',
+            'About': '''
+--------------------------------------
+               ABOUT US
+--------------------------------------
+
+Welcome to InCollege - Where Connections and Opportunities Thrive!
+
+At InCollege, we are dedicated to providing a vibrant online platform for college students to connect with friends, explore exciting career opportunities, and foster meaningful professional relationships. Our mission is to empower students like you to unleash your full potential and shape a successful future.
+
+Through our innovative features and cutting-edge technology, we strive to create a dynamic virtual space that bridges the gap between your academic journey and the professional world. Whether you're searching for internships, part-time jobs, or launching your post-graduation career, InCollege is your trusted companion.
+
+Join our vibrant community today and embark on an exciting journey of personal growth, professional development, and lifelong connections.
+
+--------------------------------------
+''',
+            'Accessibility': '''
+------------------------
+ACCESSIBILITY STATEMENT
+------------------------
+
+InCollege is committed to ensuring accessibility and inclusion for all users of our text-based app. We strive to provide a user-friendly experience for individuals with diverse abilities.
+
+Accessibility Features:
+- Clear Text Formatting: We use clear and legible text formatting to enhance readability for all users.
+- Keyboard Navigation: Our app supports keyboard navigation, allowing users to navigate through the app using keyboard shortcuts.
+- Text Resizing: You can easily adjust the text size within the app to suit your preferences.
+- Simple and Intuitive Design: Our app features a simple and intuitive design, making it easy to navigate and use.
+
+Feedback and Support:
+We value your feedback and are continuously working to improve the accessibility of our app. If you have any suggestions or encounter any barriers while using the app, please let us know. 
+
+------------------------
+''',
+            'User Agreement': '''
+------------------------
+    USER AGREEMENT
+------------------------
+
+Welcome to InCollege! This User Agreement ("Agreement") governs your use of our text-based app. By accessing or using our app, you agree to be bound by the terms and conditions outlined in this Agreement.
+
+1. Acceptance of Terms:
+   By using our app, you acknowledge that you have read, understood, and agreed to be bound by this Agreement. If you do not agree with any part of this Agreement, please refrain from using our app.
+
+3. Privacy:
+   We respect your privacy and are committed to protecting your personal information. Our Privacy Policy outlines how we collect, use, and disclose your information. By using our app, you consent to the collection and use of your data as described in our Privacy Policy.
+
+4. Limitation of Liability:
+   In no event shall InCollege or its affiliates be liable for any damages arising out of or in connection with the use of our app.
+
+5. Modification of Agreement:
+   We reserve the right to modify or update this Agreement at any time. 
+
+6. Termination:
+   We reserve the right to terminate your access to our app at any time, without prior notice, if we believe you have violated this Agreement or any applicable laws.
+
+By continuing to use our app, you acknowledge that you have read and agreed to this User Agreement.
+
+------------------------
+''',
+            'Privacy Policy': '''
+------------------------
+   PRIVACY POLICY
+------------------------
+
+At InCollege, we value your privacy and are committed to protecting your personal information. Here's a summary of our privacy practices:
+
+1. Information Collection:
+   We collect limited personal information when you register and interact with our platform.
+
+2. Data Usage:
+   We use your information to personalize your experience, deliver relevant content, and improve our services.
+
+3. Data Security:
+   We employ industry-standard security measures to protect your information from unauthorized access.
+
+4. Cookies and Tracking:
+   We may use cookies to enhance your browsing experience.
+------------------------
+
+            ''',
+            'Cookie Policy': '''
+------------------------
+   COOKIE POLICY
+------------------------
+
+At InCollege, we use cookies to enhance your browsing experience and improve our services. This Cookie Policy explains how we use cookies on our website.
+
+   We use cookies for the following purposes:
+
+   - Authentication: Cookies help us authenticate and secure your account.
+   - Preferences: Cookies remember your settings and preferences.
+   - Analytics: Cookies gather information about your usage patterns to improve our website's performance.
+   - Advertising: Cookies may be used to display relevant ads based on your interests.
+------------------------
+''',
+            'Brand Policy': '''
+------------------------
+     BRAND POLICY
+------------------------
+
+InCollege is committed to protecting its brand identity and ensuring consistent and accurate representation across all platforms. This Brand Policy outlines the guidelines for using the InCollege brand assets.
+
+   1. Logo Usage: The InCollege logo should be used in its original form and should not be altered, distorted, or modified in any way.
+   2. Colors and Typography: The official InCollege colors and typography should be used consistently to maintain brand consistency.
+   3. Prohibited Usage: The InCollege brand assets should not be used in any manner that implies endorsement, affiliation, or partnership without proper authorization.
+
+Any unauthorized usage of the InCollege brand assets is strictly prohibited.
+
+------------------------
+''',
+            'Guest Controls': '''
+------------------------
+     GUEST CONTROLS
+------------------------
+
+1. Privacy Settings: Customize the visibility of your profile and activities.
+
+2. Blocking and Reporting: Protect yourself from unwanted interactions by blocking users and reporting abuse.
+
+3. Account Deactivation: Take a break from InCollege or temporarily hide your profile.
+
+4. Password and Account Security: Strengthen your account security with strong passwords and two-factor authentication.
+
+Remember to regularly review and adjust your guest controls to suit your preferences.
+
+------------------------
+''',
+            'Languages': '''
+------------------------
+       LANGUAGES
+------------------------
+
+InCollege supports two languages:
+
+1. English: The default language of InCollege. Enjoy all features and content in English.
+
+2. Spanish: Select Spanish as your preferred language to experience InCollege in Spanish.
+
+------------------------
+'''
+        }
+        print("You selected:", link)
+        print(content[link])
+        input("Press Enter to return to the Important Links page")
+        self.importantLinks.clear()
+        return
+
+  
   ## Skills to Learn ##
   def skillA(self):
       print("Project Management")
@@ -348,6 +518,7 @@ class System:
       self.homePage.setSelection('3',{'label':'Find People I Know','action':self.findUser})
       ##self.homePage.setSelection('4',{'label':'Delete Users','action':self.deleteTable})
       self.homePage.setSelection('4',{'label':'See Our Success Video','action':self.video_menu})
+      self.homePage.setSelection('6',{'label':'InCollege Important Links','action':self.important_links})
        #For finding people you know
       ## Set Video Page Items
       self.videoMenu.setOpening("See Our Success Story:\n(Playing Video)\n")
@@ -374,7 +545,18 @@ class System:
       self.jobsMenu.setOpening("Welcome to the Job Postings Page")
       self.jobsMenu.setSelection('1',{'label':'Post Job','action':self.postJob})
       self.jobsMenu.setExitStatement("Return To Main Menu")
-      
+      # Set InCollege Important Links
+      self.importantLinks.setOpening("Welcome to the Important Links Page")
+      self.importantLinks.setSelection('1', {'label': 'Copyright Notice', 'action': lambda: self.printLink("Copyright Notice")})
+      self.importantLinks.setSelection('2', {'label': 'About', 'action': lambda: self.printLink("About")})
+      self.importantLinks.setSelection('3', {'label': 'Accessibility', 'action': lambda: self.printLink("Accessibility")})
+      self.importantLinks.setSelection('4', {'label': 'User Agreement', 'action': lambda: self.printLink("User Agreement")})
+      self.importantLinks.setSelection('5', {'label': 'Privacy Policy', 'action': lambda: self.printLink("Privacy Policy")})
+      self.importantLinks.setSelection('6', {'label': 'Cookie Policy', 'action': lambda: self.printLink("Cookie Policy")})
+      self.importantLinks.setSelection('7', {'label': 'Brand Policy', 'action': lambda: self.printLink("Brand Policy")})
+      self.importantLinks.setSelection('8', {'label': 'Guest Controls', 'action': lambda: self.printLink("Guest Controls")})
+      self.importantLinks.setSelection('9', {'label': 'Languages', 'action': lambda: self.printLink("Languages")})
+      self.importantLinks.setExitStatement("Return To Home Page")
 
 
       
