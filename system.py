bb1529f732d27c4b168b26ebceb7c31ddd736d86
makdoodie <45640071+makdoodie@users.noreply.github.com>
2023-06-17 20:01:29 +0000

    general links partially implemented. just saving

--- system.py
+++ system.py
@@ -261,6 +261,8 @@ class System:
         if hashed_inputpass == account[1]:
           self.privacyMenu.visibilities[self.privacyMenu.menuItems.index('Guest Controls')] = True
           self.importantLinks.visibilities[self.importantLinks.menuItems.index('Languages')] = True
+          self.generalMenu.visibilities[self.generalMenu.menuItems.index('Sign Up')] = True
+          
           print("You Have Successfully Logged In!")
           self.user.login(userName,account[2],account[3])
           self.home_page()
@@ -449,7 +451,10 @@ InCollege is committed to protecting its brand identity and ensuring consistent 
 Any unauthorized usage of the InCollege brand assets is strictly prohibited.
 
 ------------------------
-'''}
+''',
+            'Help Center': '''
+            We're here to help
+            '''}
         print("You selected:", link)
         print(content[link])
         input("Press Enter to return to the Important Links page")
@@ -582,3 +587,6 @@ At InCollege, we value your privacy and are committed to protecting your persona
       self.usefulLinks.addItem('Business Solutions', self.solutions)
       self.usefulLinks.addItem('Directories', self.directories)
       #General links menu navigation links
+      self.generalMenu.setOpening('General Links')
+      self.generalMenu.addItem('Sign Up', self.join_menu , False) #finish this
+      self.generalMenu.addItem('Help Center', lambda: self.printLink("Help Center"))
