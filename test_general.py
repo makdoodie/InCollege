124f454dd5ba626759dddd5a547416ae78117a4e
asibai7 <135031679+asibai7@users.noreply.github.com>
2023-06-19 21:53:27 +0000

    completed testing for general info

--- test_general.py
+++ test_general.py
@@ -70,7 +70,7 @@ def test_signup1(system_instance, temp_remove_accounts, capsys): #tests that reg
   assert type(result) == tuple
   assert result[0] == 'ahmad' and result[1] == system_instance.encryption('Asibai1$') and result[2] == 'ah' and result[3] == 'mad'
 
-def test_signup2(system_instance, name_register, capsys): #test that a registered  user can login from the general signup option
+def test_signup2(system_instance, name_register, capsys): #tests that a registered  user can login from the general signup option
   input = ['5', '1', '1', '1', 'ahmad', 'Asibai1$', '0', '0', '0', '0', '0']
   with mock.patch('builtins.input', side_effect=input):
     system_instance.home_page()
@@ -81,7 +81,7 @@ def test_signup2(system_instance, name_register, capsys): #test that a registere
   assert output[44] == 'Enter Password: '
   assert output[45] == 'You Have Successfully Logged In!'
 
-def test_helpcenter(system_instance, temp_remove_accounts, capsys): 
+def test_helpcenter(system_instance, temp_remove_accounts, capsys): #tests that the help center can be accessed from the general useful links option
   input = ['5', '1', '2', '0', '0', '0', '0']
   with mock.patch('builtins.input', side_effect=input):
     system_instance.home_page()
@@ -89,8 +89,42 @@ def test_helpcenter(system_instance, temp_remove_accounts, capsys):
   output = captured.out.split('\n')
   assert output[37] == "We're here to help"
 
-# [3] About
-# [4] Press
-# [5] Blog
-# [6] Careers
-# [7] Developers
+def test_about(system_instance, temp_remove_accounts, capsys): #tests that about can be accessed from the general useful links option
+  input = ['5', '1', '3', '0', '0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out.split('\n')
+  assert output[37] == "In College: Welcome to In College, the world's largest college student"
+
+def test_press(system_instance, temp_remove_accounts, capsys): #tests that the press can be accessed from the general useful links option
+  input = ['5', '1', '4', '0', '0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out.split('\n')
+  assert output[37] == 'In College Pressroom: Stay on top of the latest news, updates, and reports'
+
+def test_blog(system_instance, temp_remove_accounts, capsys): #tests that the blog can be accessed from the general useful links option
+  input = ['5', '1', '5', '0', '0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out.split('\n')
+  assert output[36] == 'Under Construction'
+  
+def test_careers(system_instance, temp_remove_accounts, capsys): #tests that the careers section can be accessed from the general useful links option
+  input = ['5', '1', '6', '0', '0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out.split('\n')
+  assert output[36] == 'Under Construction'
+  
+def test_developers(system_instance, temp_remove_accounts, capsys): #tests that the developers section can be accessed from the general useful links option
+  input = ['5', '1', '7', '0', '0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out.split('\n')
+  assert output[36] == 'Under Construction'
