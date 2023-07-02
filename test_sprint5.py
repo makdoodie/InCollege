efcaa2c9bb836a602cbbe42abc5d6a4b65c36a74
aveltri-usf <82551323+aveltri-usf@users.noreply.github.com>
2023-07-02 00:51:54 +0000

    Added 'clear and restore database'  and 'system instance' fixtures.

---
+++ test_sprint5.py
@@ -0,0 +1,47 @@
+import pytest
+from unittest import mock
+from system import System
+
+
+@pytest.fixture
+def system_instance():
+  """Creates and instance of the system performs some menu initialization."""
+  s1 = System()
+  s1.initMenu()
+  return s1
+
+
+@pytest.fixture
+def clear_restore_db(system_instance): 
+  """Sets up the database for testing by saving and clearing any persistent records, 
+  after a test finishes test records are cleared and saved records are restored."""
+  data = {}
+  # tables with FKs referencing other tables should come after the referenced table
+  tables = ['accounts', 'friends', 'experiences', 'jobs']
+  # store each of the tables into a dictionary
+  for table in tables:
+    system_instance.cursor.execute(f"SELECT * FROM {table}")
+    data[table] = system_instance.cursor.fetchall()
+
+  # delete all records from the the accounts table, 
+  # and should auto delete all records from tables with FK to accounts
+  if len(data[tables[0]]):
+    system_instance.cursor.execute("DELETE FROM accounts")
+    system_instance.conn.commit()
+  # delete all records from the jobs table
+  if len(data[tables[-1]]):
+    system_instance.cursor.execute("DELETE FROM jobs")
+  system_instance.conn.commit()
+    
+  yield
+  # delete any testing records from the database 
+  system_instance.cursor.execute("DELETE FROM accounts")
+  # restore saved records to all tables 
+  for table in tables:
+    if len(data[table]):
+      # add a ? to the list of parameters for each column in the table
+      parameters = f"({','.join('?' for col in data[table][0])})"
+      query = f"INSERT INTO {table} VALUES {parameters}"
+      system_instance.cursor.executemany(query, data[table])
+  system_instance.conn.commit()
+
