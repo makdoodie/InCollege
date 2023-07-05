291b3a3affd97f58aa7284faddffbb84d438baec
aveltri-usf <82551323+aveltri-usf@users.noreply.github.com>
2023-07-05 23:35:40 +0000

    All sprint 5 tests added into 1 file.

--- test_sprint5.py
+++ test_sprint5.py
@@ -92,6 +92,28 @@ def test_instance_1():
     return system
 
 
+@pytest.fixture #test that user can input first and last name when registering
+def name_register(system_instance, clear_restore_db, capsys):
+  inputs = ['2', 'ahmad', 'ah', 'mad', 'usf', 'cs', 'Asibai1$', 'Asibai1$', 'ahmad', 'Asibai1$', '0', '0']
+  with mock.patch('builtins.input', side_effect=inputs):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out
+  assert 'Account created successfully.' in output
+  yield
+  
+
+@pytest.fixture #Creates a second account to test with in the database
+def name_register_1(system_instance, clear_restore_db, capsys):
+  inputs = ['2', 'makdoodie', 'mahmood', 'sales','usf','cs', 'Test123!', 'Test123!', 'makdoodie', 'Test123!', '0', '0']
+  with mock.patch('builtins.input', side_effect=inputs):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out
+  assert  'Account created successfully.' in output
+  yield
+
+
 #============================================== Story 1 Tests ======================================================
 
 def test_accounts_schema(system_instance):
@@ -246,7 +268,7 @@ def test_integrated_has_profile(system_instance, clear_restore_db):
   system_instance.user.hasProfile() == False
 
 
-def test_display_profile():
+def test_display_profile_user(): # previously test_display_profile but renamed to avoid test conflict
   """
   Tests the output of the user's display profile function in both partial and full modes.
   The test user information used in this function was generated with the assistance of ChatGPT.
@@ -670,3 +692,133 @@ Experience 3
   assert query3 in output
   assert query4 in output
   assert query5 in output
+
+
+# ============================================== Story 4 Tests ====================================================
+
+#Epic 5, Story 4: I want to be able to see the profile of any friend I have.
+
+#Subtask 1: In Show my Network, you may click on any connection and have the option to view their profile in addition to the already created disconnect and exit options, this is only an option if the connection has created a profile
+def test_friend_profile(system_instance, clear_restore_db, capsys, name_register ,name_register_1):
+  #adds a friendship between the users
+  system_instance.cursor.execute('INSERT INTO friends (sender, receiver, status) VALUES ("ahmad", "makdoodie", "accepted");')
+  #makes sure there's no view profile option
+  input = ['1', 'ahmad', 'Asibai1$', '3', '2', '1', '0' '0', '0','0', '0','0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out
+  assert '[1] View Profile' not in output
+  #logs in and adds things to profile
+  input = ['1', 'makdoodie', 'Test123!', '1', '1','1', 'placeholder title', '0', '0', '0','0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  #now that profile options are added, checks that view profile button is present
+  input = ['1', 'ahmad', 'Asibai1$', '3', '2', '1', '0' '0', '0','0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out
+  assert '[1] View Profile' in output
+
+
+#Subtask 2: Clicking on a connection should not display the user’s profile automatically
+def test_display_profile(system_instance, clear_restore_db, capsys, name_register ,name_register_1):
+  #adds a friendship between the users
+  system_instance.cursor.execute('INSERT INTO friends (sender, receiver, status) VALUES ("ahmad", "makdoodie", "accepted");')
+  #logs in and adds things to profile
+  input = ['1', 'makdoodie', 'Test123!', '1', '1','1', 'placeholder title', '0', '0', '0','0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+    input = ['1', 'ahmad', 'Asibai1$', '3', '2', '1', '0' '0', '0','0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out
+  assert 'placeholder title' not in output
+
+
+#Subtask 3: Once view profile is pressed, that connection’s profile will be displayed and so 0 (an exit) will take you back to the previous menu
+def test_view_friend(system_instance, clear_restore_db, capsys, name_register ,name_register_1):
+  #adds a friendship between the users
+  system_instance.cursor.execute('INSERT INTO friends (sender, receiver, status) VALUES ("ahmad", "makdoodie", "accepted");')
+  #adds some sample data to the user's profile
+  sql = "UPDATE accounts SET yearsAttended=5, title='Software Engineer Intern', infoAbout='Just a bit about me', profile = True WHERE username='makdoodie';"
+  system_instance.cursor.execute(sql)
+  input = ['1', 'ahmad', 'Asibai1$', '3', '2', '1', '1', '0', '0' '0', '0','0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  captured = capsys.readouterr()
+  output = captured.out.split("\n")
+  with open("out.txt", 'w') as f:
+    for line in output:
+      f.write(f"{line}\n")
+  
+  assert output[55] == 'Title: Software Engineer Intern'
+  assert output[56] == 'About: Just a bit about me'
+  assert output[63] == ' Years Attended: 5'
+  #Makes sure exit will take you back to the previous menu
+  assert output[78] == 'Exiting'
+
+
+# ============================================= Story 6 Tests ======================================================
+# Epic 5, Story 6: Edit load process after a user log in
+
+#Subtask 1: Load year attended (education: INT), title (varchar 50), info (text)) from the accounts table after a user log in
+def test_loads_info(system_instance, name_register, capsys):
+  #logs in and adds things to profile
+  input = ['1', 'ahmad', 'Asibai1$', '1', '1','1', 'Software Engineer Intern', '0', '2', 'Just a bit about me', '0', '3','3','5','0', '0', '0','0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  #throwing away output so it doesn't make false positive later
+  captured = capsys.readouterr()
+  output = captured.out
+  #logs in and views profile
+  input = ['1', 'ahmad', 'Asibai1$', '1', '2', '0', '0','0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  #confirms all data in database is shown when user views profile
+  captured = capsys.readouterr()
+  output = captured.out
+  assert 'Years Attended: 5' in output
+  assert 'Software Engineer Intern' in output
+  assert 'Just a bit about me' in output
+
+
+#Subtask 2: Load all experiences (title, employer, date started, date ended, location, and then a description of what they did (text)) from the experience table
+def test_load_experiences(system_instance, name_register, capsys):
+  #Makes it so the user has a profile and adds an experience
+  system_instance.cursor.execute("UPDATE accounts SET profile = True WHERE username='ahmad';")
+  system_instance.cursor.execute("INSERT INTO experiences (expID, username, title, employer, dateStarted, dateEnded, location, description) VALUES (1, 'ahmad', 'Software Developer', 'ABC Corp', '2020-01-19', '2020-06-21', 'New York, NY', 'Developed and maintained software for a variety of corporate functions.');")
+  #logs in and views profile
+  input = ['1', 'ahmad', 'Asibai1$', '1', '2', '0', '0','0', '0', '0']
+  with mock.patch('builtins.input', side_effect=input):
+    system_instance.home_page()
+  #confirms all data in database is shown when user views profile
+  captured = capsys.readouterr()
+  output = captured.out
+  assert """Title: Software Developer
+ Employer: ABC Corp
+ Start Date: 2020-01-19
+ End Date: 2020-06-21
+ Location: New York, NY
+ Description: Developed and maintained software for a variety of corporate functions.""" in output
+
+
+#Subtask 3: Edit loadacceptedfriends to load all profile information
+def test_loadacceptedfriends(system_instance, clear_restore_db, capsys, name_register ,name_register_1):
+    #adds a friendship between the users
+    system_instance.cursor.execute('INSERT INTO friends (sender, receiver, status) VALUES ("ahmad", "makdoodie", "accepted");')
+    #inserts profile information for one user
+    username = "makdoodie"
+    sql = "UPDATE accounts SET yearsAttended=5, title='Software Engineer Intern', infoAbout='Just a bit about me', profile = True WHERE username=?;"
+    system_instance.cursor.execute(sql, (username,))
+    with mock.patch('builtins.input', side_effect=['ahmad', 'Asibai1$', '0']):
+      system_instance.login()
+    system_instance.loadAcceptedFriends()
+    assert 'makdoodie' in system_instance.user.acceptedRequests
+    makdoodie = system_instance.user.acceptedRequests['makdoodie']
+    system_instance.loadFriendProfile(makdoodie)
+    assert makdoodie.Profile.headline == "Software Engineer Intern"
+    assert makdoodie.Profile.about == "Just a bit about me"
+    assert makdoodie.Profile.education.yearsAttended == 5
